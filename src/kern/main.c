d4787bb82c8ac6398a3d878cb8fe525a1512603f
William Woodruff <william@tuffbizz.com>
2014-04-23 22:04:16 -0400

    added some constants, removed comment

--- src/kern/main.c
+++ src/kern/main.c
@@ -1,11 +1,15 @@
 /*	main.c
 	The entry point of the OS. 
-	Loaded by boot.asm.
 	Author: William Woodruff
 */
 
+#define VIDEO_ADDRESS 0xb8000
+#define VGA_ROWS 25
+#define VGA_COLS 80
+#define WHITE_ON_BLACK 0x0f
+
 void main(void)
 {
-	char* video_memory = (char*) 0xb8000;
-	*video_memory = 'X';
+	char* video_memory = (char*) VIDEO_ADDRESS;
+	*video_memory = 'Y';
 }

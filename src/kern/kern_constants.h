9427c6a220d46a678d70b73bccc3104ba1656e0d
Pawel Czarnecki <pan.pawel@outlook.com>
2014-05-05 14:27:07 -0400

    Create kern_constants.h
    This will store compile-time definitions for the kernel

---
+++ src/kern/kern_constants.h
@@ -0,0 +1,10 @@
+#IF_NOT_DEF KERN_CONSTANTS_H
+#define KERN_CONSTANTS_H
+
+//all sizes in byes
+
+#define STACK_SIZE 8192    //8Kb, should be increased later
+#define INIT_MAT_SIZE 1024   //1024 entries into the Memory Allocation Table
+
+
+#ENDIF

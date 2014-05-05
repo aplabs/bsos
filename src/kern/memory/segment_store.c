6ac7a1dfc52352e0ba54e06ee72a84acc3e5ba5f
Pawel Czarnecki <pan.pawel@outlook.com>
2014-05-05 15:09:25 -0400

    Update segment_store.c
    further planning necessary..

--- src/kern/memory/segment_store.c
+++ src/kern/memory/segment_store.c
@@ -3,10 +3,34 @@
 */
 #include "../kern_constants"
 
+//function prototypes (to be put into own header file)
+
 int __insert_segment(struct* __memory_segment);
+int __free_address(void* __address);
+
 
 struct __Memory_Allocation_Table{
-  void* _indices[INIT_MAT_SIZE]; 
+  void* _indices[INIT_MAT_SIZE + 1];     //the last index holds the location of the next MAT
   void* _entries[INIT_MAX_SIZE * BLOCK_SIZE]
-};
+  unsigned int _num_entries = 0;
+  unsigned int _last_free_slot = 0;  //the last location freed will be the first one used, set to _num_entries once used
+} __init_MAT;
 
+
+/*
+   Ideally it should be inserted to keeop the order of the addresses in line
+*/
+int __insert_segment(struct* __memory_segment){
+   if(__num_entries == INIT_MAX_SIZE)     //we have reached our limit
+     return -1;
+     
+     
+     //insert_index should be selected based on order of real addresses
+     //insert_segemnt should be the last freed slot or at the very end
+   unsigned int insert_index = __num_entries;
+   unsigned int insert_segment = _last_free_slot != _num_entires ? _last_free_slot : _num_entries;
+   _last_free_slot = _num_entries
+   __init_MAT._indices[insert_index] = 
+   
+   
+}

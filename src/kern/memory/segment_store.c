6db04b609fb6c734dc700b81093af82e201d46fe
Pawel Czarnecki <pan.pawel@outlook.com>
2014-05-05 14:38:27 -0400

    Update segment_store.c

--- src/kern/memory/segment_store.c
+++ src/kern/memory/segment_store.c
@@ -1,3 +1,12 @@
 /* segment_store.c will define code that keeps track of kernel-defined memory segments
    -Pawel
 */
+#include "kern_constants"
+
+int __insert_segment(struct* __memory_segment);
+
+struct __Memory_Allocation_Table{
+  void* _indices[INIT_MAT_SIZE]; 
+  void* _entries[INIT_MAX_SIZE * BLOCK_SIZE]
+};
+

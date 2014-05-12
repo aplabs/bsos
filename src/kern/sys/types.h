64ec3b4307579a91de0d6e1269958210ad6a5d89
William Woodruff <william@tuffbizz.com>
2014-05-11 20:42:43 -0400

    added prelim types.h

---
+++ src/kern/sys/types.h
@@ -0,0 +1,16 @@
+/*	types.h
+	Defines macros for common types in bsos.
+	Author: William Woodruff, 2014
+*/
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#define byte char
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+#endif /* TYPES_H */

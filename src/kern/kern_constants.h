967ca4de2c29facb1b3a183870fa1d7cd54b08f6
William Woodruff <william@tuffbizz.com>
2014-05-20 20:56:05 -0400

    more migrations, fixed some sizes (we don't know that we're in 64bit mode

--- src/kern/kern_constants.h
+++ src/kern/kern_constants.h
@@ -11,15 +11,4 @@
 #define STACK_SIZE 8192    //8Kb, should be increased later
 #define INIT_MAT_SIZE 1024   //1024 entries into the Memory Allocation Table
 
-#define CHAR_SIZE     1 //We're using x86 architecture, so I thought it would be a good idea to put these in.
-#define SHORT_SIZE    2
-#define INT_SIZE      4
-#define LONG_SIZE     8
-#define LONG_SIZE_LOG2 3
-#define LONG_LONG_SIZE 8
-#define PTR_SIZE      8
-#define PTR_BITS      48
-#define ADR_BITS      48
-#define PAGE_SIZE_LOG2 12
-
 #endif /* KERN_CONSTANTS_H */
--- src/kern/sys/types.h
+++ src/kern/sys/types.h
@@ -1,6 +1,6 @@
 /*	types.h
 	Defines macros for common types in bsos.
-	Author: William Woodruff, 2014
+	Author: William Woodruff, Alex Gould (code migrated)
 */
 
 #ifndef TYPES_H
@@ -12,5 +12,15 @@ typedef unsigned char uchar;
 typedef unsigned short ushort;
 typedef unsigned int uint;
 typedef unsigned long ulong;
+
+#define CHAR_SIZE     1 
+#define SHORT_SIZE    2
+#define INT_SIZE      4
+#define LONG_SIZE     4 /* assume all longs are 32-bit until 64-bit mode asserted */
+#define LONG_LONG_SIZE 8
+#define PTR_SIZE      4 /* same logic as LONG_SIZE, assume 32-bit pts until asserted */
+#define PTR_BITS      48
+#define ADR_BITS      48
+#define PAGE_SIZE_LOG2 12
 
 #endif /* TYPES_H */

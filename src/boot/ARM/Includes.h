036fb47c371ecf45bee9345afe5bcf6693ef9018
lyra833 <alxg833@gmail.com>
2014-06-07 00:23:35 -0400

    Screw it, I'm exhausted.

---
+++ src/boot/ARM/Includes.h
@@ -0,0 +1,15 @@
+#ifndef INCLUDES
+#define INCLUDES
+
+#include <stdio.h>
+#include <Headers/si32_device.h>
+#include <Headers/SI32_CLKCTRL_A_Type.h>
+#include <Headers/SI32_FLASHCTRL_A_Type.h>
+#include <Headers/SI32_VMON_A_Type.h>
+#include <Headers/SI32_RSTSRC_A_Type.h>
+#include <Headers/SI32_CLKCTRL_A_Type.h>
+#include <Headers/SI32_UART_A_Type.h>
+#include <Headers/SI32_PBSTD_A_Type.h>
+#include <Headers/SI32_PBCFG_A_Type.h>
+
+#endif  //INCLUDES.HPP

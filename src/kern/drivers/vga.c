cdc1001a1b4fbc301ad268c6491423d1868a9543
William Woodruff <william@tuffbizz.com>
2014-06-20 13:14:43 -0400

    refined vga

--- src/kern/drivers/vga.c
+++ src/kern/drivers/vga.c
@@ -5,84 +5,22 @@
 
 #include "vga.h"
 
-/*	get_cursor
-	get the current offset of the cursor in memory
-*/
-int get_cursor(void)
-{
-	port_byte_out(REG_SCREEN_CTRL, 0x0e);
-	int offset = port_byte_in(REG_SCREEN_DATA) << 8;
-	port_byte_out(REG_SCREEN_CTRL, 0x0f);
-	offset += port_byte_in(REG_SCREEN_DATA);
+unsigned char* video_memory = (unsigned char*) VIDEO_MEMORY;
+unsigned short cursor_pos = 0;
 
-	return offset * 2;
+void printch(char ch)
+{
+	video_memory[cursor_pos * 2] = ch;
+	cursor_pos++;
 }
 
-/*	set_cursor
-	set the position of the cursor to the given offset
-*/
-void set_cursor(int offset)
+void prints(char* str)
 {
-	offset /= 2;
-	port_byte_out(REG_SCREEN_CTRL, 0x0f);
-	port_byte_out(REG_SCREEN_DATA, (unsigned char) offset & 0xff);
-	port_byte_out(REG_SCREEN_CTRL, 0x0e);
-	port_byte_out(REG_SCREEN_DATA, (unsigned char) ((offset >> 8) & 0xff));
-}
-
-/*	get_screen_offset
-	return the memory offset representative of the given VGA row/column pair
-*/
-int get_screen_offset(int row, int col)
-{
-	return ((row * 80) + col) * 2;
-}
-
-/*	__write_str
-	write a null-terminated string to the screen, setting the cursor after the string
-*/
-void __write_str(char attr, const char* str)
-{
-	unsigned char* video_memory = (unsigned char*) VIDEO_MEMORY;
-
 	while (*str)
 	{
-		*video_memory++ = *str++;
-		*video_memory++ = attr;
+		printch(*str);
+		str++;
 	}
-
-
-	set_cursor((int) (video_memory - VIDEO_MEMORY));
-}
-
-/*	__print_str
-	do the same as __write_str, but prints at the cursor location
-	and new line with '\n' character
-*/
-void __print_str(char attr, const char* str)
-{
-	unsigned char* video_memory = (unsigned char*) VIDEO_MEMORY;
-	video_memory += get_cursor();
-
-	while (*str)
-	{
-		if(*str == NEWLINE)
-		{
-			int offset = (int) (video_memory - VIDEO_MEMORY);
-			offset = offset % (VGA_COLS * 2);
-			offset = (VGA_COLS * 2) - offset;
-			video_memory += offset;
-			str++;
-		}
-		else
-		{
-			*video_memory++ = *str++;
-			*video_memory++ = attr;
-		}
-	}
-
-
-	set_cursor((int) (video_memory - VIDEO_MEMORY));
 }
 
 /*	__print_binary
@@ -100,7 +38,7 @@ void __print_binary(char attr, unsigned char b){
 		else
 			*++str = '1';
 	}
-	__print_str(attr, loc);
+	prints(loc);
 }
 
 /*	__print_hex
@@ -118,7 +56,7 @@ char key[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}
 	*++str = key[l];
 
 	//*++str = '\n'; //newline
-	__print_str(attr, loc);
+	prints(loc);
 }
 
 /*	clear_screen
@@ -126,13 +64,8 @@ char key[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'}
 */
 void clear_screen(void)
 {
-	unsigned char* video_memory = (unsigned char*) VIDEO_MEMORY;
-
-	for (int i = 0; i < 2000; i++)
+	for (int i = 0; i <= 8000; i += 2)
 	{
-		*video_memory++ = ' ';
-		*video_memory++ = 0x0F;
+		video_memory[i] = ' ';
 	}
-
-	set_cursor(get_screen_offset(0, 0));
 }
--- src/kern/drivers/vga.h
+++ src/kern/drivers/vga.h
@@ -9,11 +9,8 @@
 #include "vga_constants.h"
 #include "port_io.h"
 
-int get_cursor(void);
-void set_cursor(int offset);
-int get_screen_offset(int row, int col);
-void __write_str(char attr, const char* str);
-void __print_str(char attr, const char* str);
+void printch(char ch);
+void prints(char* str);
 void __print_binary(char attr, unsigned char c);
 void __print_hex(char attr, unsigned char c);
 void clear_screen(void);

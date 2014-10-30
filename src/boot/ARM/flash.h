1e3ef6b4cfef0990b8f99e7f3ef914bee22d5982
William Woodruff <william@tuffbizz.com>
2014-10-30 19:07:29 -0400

    commit @ Thu Oct 30 19:07:29 EDT 2014

--- src/boot/ARM/flash.h
+++
@@ -1,8 +0,0 @@
-#ifndef __FLASH_H__
-#define __FLASH_H__
-#include <stdbool.h>
-
-void clear(unsigned int addr);
-int write(unsigned char* buf,unsigned int addr, unsigned int num);
-void start(void);
-#endif
